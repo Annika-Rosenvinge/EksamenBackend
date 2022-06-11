b2ed92b7ed82afb7b992e1d12a199d900c92e300
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2022-06-11 14:31:40 +0200

    UserDTO written

--- src/main/java/dtos/UserDTO.java
+++ src/main/java/dtos/UserDTO.java
@@ -1,7 +1,9 @@
 package dtos;
 
 import entities.Role;
+import entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDTO {
@@ -13,6 +15,54 @@ public class UserDTO {
 
     }
 
-    
+    public UserDTO (String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
 
+    public UserDTO (User user){
+        this.userName = user.getUserName();
+        this.password = user.getUserPass();
+    }
+
+    public static  List<UserDTO> getDtos (List<User> users){
+        List<UserDTO> userDTOS = new ArrayList<>();
+        if (users != null){
+            users.forEach(user -> userDTOS.add(new UserDTO(user)));
+        }
+        return userDTOS;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", roleList=" + roleList +
+                '}';
+    }
 }
